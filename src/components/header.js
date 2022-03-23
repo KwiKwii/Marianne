e4b46fb4b14f6592b5705090260bfcc98e327564
KwiKwii <DominikMarciniak69@gmail.com>
2022-03-23 19:20:20 +0100

    sekcja header

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <i className="fa-solid fa-bars"></i>
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <i className="fa-solid fa-user"></i>
+                <i className="fa-solid fa-basket-shopping"></i>
+            </div>
+            <div className="header_image"/>
+        </>
+    )
+}
+
+export default Header;
