515ed27450f115c4fb85e8b07b9c4af7c7c3ed19
KwiKwii <DominikMarciniak69@gmail.com>
2022-03-24 20:11:49 +0100

    search

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,20 @@
 import React, {useState} from "react";
 import Menu from "./menu";
+import Serach from "./serach";
 
 const Header = () => {
-    const [toggleHidden, setToggleHidden] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [toggleSearch, setToggleSearch] = useState(false);
     return (
         <>
+            {toggleSearch ? <Serach/> : null}
             <div className="header">
-                <i className="fa-solid fa-bars" onClick={() => setToggleHidden(!toggleHidden)}></i>
-                <i className="fa-solid fa-magnifying-glass"></i>
+                <i className="fa-solid fa-bars" onClick={() => setToggleMenu(!toggleMenu)}></i>
+                <i className="fa-solid fa-magnifying-glass" onClick={() => setToggleSearch(!toggleSearch)}></i>
                 <i className="fa-solid fa-user"></i>
                 <i className="fa-solid fa-basket-shopping"></i>
             </div>
-            {toggleHidden ? <Menu/> : null}
+            {toggleMenu ? <Menu/> : null}
             <div className="header_image"/>
         </>
     )
---
+++ src/components/serach.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Serach = () => {
+    return (
+        <>
+            <input type="text" name="search" id="search" placeholder="Szukaj w sklepie..."/>
+            <i className="fa-solid fa-magnifying-glass"></i>
+        </>
+
+    )
+}
+
+export default Serach;
